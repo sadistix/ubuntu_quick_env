24087edabe8b67f1039fe661d7b23ede8e5e0484
111 <111>
2025-12-28 21:02:26 +0300

    Add minikube, zsh, brew, helm

--- script.sh
+++ script.sh
@@ -2,6 +2,9 @@ sudo apt update
 
 #Set up Docker's apt repository.
 
+sleep 2
+echo "\n\nDocker installation\n\n" 
+sleep 2
 # Add Docker's official GPG key:
 sudo apt update
 sudo apt install ca-certificates curl
@@ -28,18 +31,20 @@ sudo systemctl start docker
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
-#Verify docker installation
-sleep 1
-echo "\n verify installation: \n"
-sleep 1
+#Verify-installation
+sleep 3
+echo "\n\nVerify installation\n\n"
 docker run hello-world
-sleep 1
+sleep 4 
+
+
 
 #kubectl installation
-
+sleep 2 
+echo "Kubectl installation"
+sleep 2
 sudo apt-get update
 # apt-transport-https may be a dummy package; if so, you can skip that package
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
@@ -54,8 +59,38 @@ echo "\n verify installation: \n"
 kubectl cluster-info
 
 #Install munikube
+sleep 2
+echo "\n\n minikube installation:\n\n"
+sleep 2
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
+#check minikube installation
+sleep 3 
+echo "\n\nVerify minikube installation\n\n"
+minikube --info
+sleep 3
 
+
+#install zsh & oh my zsh
+echo "zsh and oh my zsh instllation\n\n"
+sleep 2
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+#Install brew
+echo "\n\nhomebrew installation:\n\n"
+sleep 2
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /home/ubuntu/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
+
+#install helm
+echo "\n\n helm installation:\n\n"
+sleep 2
+brew install helm
 
 
